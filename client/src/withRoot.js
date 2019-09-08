43d78f3ff9f404b8da05f805d8875f688d88811e
nick pai <>
2019-09-08 03:04:05 -0400

    Revert "merge"
    This reverts commit 8af34fa9e008c66048f883fe5f57c5cbf85f08b5, reversing
    changes made to 8adfc7b24b73e2b4b544f7cc11c39865fcc799f7.

---
+++ client/src/withRoot.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { grey, deepPurple } from '@material-ui/core/colors';
+
+// A theme with custom primary and secondary color.
+// It's optional.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: deepPurple[300],
+      main: deepPurple[500],
+      dark: deepPurple[700],
+    },
+    secondary: {
+      light: grey[300],
+      main: grey[500],
+      dark: grey[700],
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+function withRoot(Component) {
+  function WithRoot(props) {
+    // MuiThemeProvider makes the theme available down the React tree
+    // thanks to React context.
+    return (
+      <MuiThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;
